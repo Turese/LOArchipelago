678eb86631a48cf543849d94fb6c3b67c6b363e3
Turese Anderson <25139527+Turese@users.noreply.github.com>
2026-08-10 15:50:22 -0700

    update how game items are classified

--- worlds/look_outside/items.py
+++ worlds/look_outside/items.py
@@ -36,8 +36,8 @@ def check_gate_classification_by_options(item: str, options: LookOutsideOptions)
         else:
             return ItemClassification.useful
     if item in item_name_groups["USEFUL_SKILL_VIDEO_GAME"]:
-        if options.include_game_skills:
-            return ItemClassification.progression
+        if not options.include_game_skills:
+            return ItemClassification.useful
     if item in item_name_groups["KOTD_FIGURE"]:
         if options.include_superbosses:
             return ItemClassification.progression
